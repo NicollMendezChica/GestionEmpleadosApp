06f305fc3943fadbcad58efd14168d5e407e944b
NicollMendezChica <80913505+NicollMendezChica@users.noreply.github.com>
2021-12-01 16:00:27 -0500

    Heroku

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,16 @@ var app = require('./app');
 var mogoose = require('./src/conexDB/conn')
 var port = 4000;
 
+require('dotenv').config({path:'variables.env'});
+console.log(process.env.DB_URL);
+
 app.listen(port, () =>{
     console.log("servidor corriendo ok")
+});
+
+const host= process.env.HOST || '0.0.0.0';
+const port=process.env.PORT || 3000;
+
+app.listen(port, host, () =>{
+    console.log('El servidor está funcionado')
 });
